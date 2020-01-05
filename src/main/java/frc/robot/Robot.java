b18c34b8d5198fbef3abf271b17a1fb1c6929639
David <dlemasurier@comcast.net>
2020-01-04 19:30:41 -0500

    added camera and ADIS16448 gyro for testing

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,6 +1,9 @@
 
 package frc.robot;
 
+import com.analog.adis16448.frc.ADIS16448_IMU;
+//import com.analog.adis16470.frc.ADIS16470_IMU;
+
 import org.opencv.core.Mat;
 import org.opencv.imgproc.Imgproc;
 
@@ -8,10 +11,14 @@ import edu.wpi.cscore.CvSink;
 import edu.wpi.cscore.CvSource;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
 import frc.robot.subsystems.Drive;
+import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 
@@ -25,6 +32,13 @@ public class Robot extends TimedRobot {
 
 	public static OI oi;
 	public static Drive drive = new Drive();
+
+	//public static ADIS16448_IMU imu = new ADIS16448_IMU();
+	public static final AnalogGyro gyro = new AnalogGyro(1);
+
+	public static ExampleSubsystem m_subsystem = new ExampleSubsystem();
+
+	//private double imuOffset=0;
 	
 	/**
 	 * This function is run when the robot is first started up and should be used
@@ -53,6 +67,9 @@ public class Robot extends TimedRobot {
 				outputStream.putFrame(output);
 			}
 		}).start();
+
+		//imuOffset = imu.getAngle();
+		gyro.calibrate();
 	}
 
 	public void disabledInit() {
@@ -63,6 +80,7 @@ public class Robot extends TimedRobot {
 	}
 
 	public void autonomousInit() {
+		//imuOffset = imu.getAngle();
 	}
 
 	/**
@@ -73,7 +91,8 @@ public class Robot extends TimedRobot {
 	}
 
 	public void teleopInit() {
-		oi.startDriveCommand();	
+		//imuOffset = imu.getAngle();
+		gyro.reset();
 	}
 
 	/**
@@ -81,13 +100,38 @@ public class Robot extends TimedRobot {
 	 */
 	public void teleopPeriodic() {
 		Scheduler.getInstance().run();		
+
+		// SmartDashboard.putNumber("IMU", imu.getAngle());
+		// SmartDashboard.putNumber("IMU Gyro-X", imu.getAngleX());
+		// SmartDashboard.putNumber("IMU Gyro-Y", imu.getAngleY());
+		// SmartDashboard.putNumber("IMU Gyro-Z", imu.getAngleZ());
+		
+		// SmartDashboard.putNumber("IMU Accel-X", imu.getAccelX());
+		// SmartDashboard.putNumber("IMU Accel-Y", imu.getAccelY());
+		// SmartDashboard.putNumber("IMU Accel-Z", imu.getAccelZ());
+		
+		// SmartDashboard.putNumber("IMU Pitch", imu.getPitch());
+		// SmartDashboard.putNumber("IMU Roll", imu.getRoll());
+		double angle = gyro.getAngle();
+		SmartDashboard.putNumber("angle", angle);
+		//SmartDashboard.putNumber("offset", imuOffset);
+		//SmartDashboard.putNumber("robot_angle", angle-imuOffset);
+		if (angle > -100)
+		{
+		  m_subsystem.turnonLed();
+		}		
+		// SmartDashboard.putNumber("IMU Pressure: ", imu.getBarometricPressure());
+		// SmartDashboard.putNumber("IMU Temperature: ", imu.getTemperature()); 
+
+
+//		SmartDashboard.putNumber("Gyro", gyro.getAngle());
 	}
 
 	/**
 	 * This function is called periodically during test mode
 	 */
 	public void testPeriodic() {
-		LiveWindow.run();
+		//LiveWindow.run();
 	}
 	
 }

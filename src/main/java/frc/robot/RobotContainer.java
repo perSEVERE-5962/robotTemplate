5cbbb41436cefe6b5cfa4eb04f2f790d44f11744
David <david.lemasurier@gmail.com>
2020-01-05 15:05:25 -0500

    converting to new 2020 command-based

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,77 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.commands.AutoCommand;
+import frc.robot.commands.RunTankDrive;
+import frc.robot.subsystems.Drive;
+import edu.wpi.first.wpilibj2.command.Command;
+
+/**
+ * This class is where the bulk of the robot should be declared.  Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
+ * (including subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  private final Joystick driverController = new Joystick(0);
+  private final Joystick copilotController = new Joystick(1);
+
+  // The robot's subsystems and commands are defined here...
+  private final Drive driveSubsystem = new Drive(driverController);
+  private final AutoCommand autoCommand = new AutoCommand(driveSubsystem);
+  private final RunTankDrive driveCommand = new RunTankDrive(driveSubsystem);
+  
+  
+
+  /**
+   * The container for the robot.  Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() {
+    // Configure the button bindings
+    configureButtonBindings();
+  }
+
+  /**
+   * Use this method to define your button->command mappings.  Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
+   * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+
+  }
+
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return autoCommand;
+  }
+
+  public Command getDriveCommand() {
+    return driveCommand;
+  }
+
+  public Joystick getDriverJoystick() {
+    return driverController;
+  }
+
+  public Joystick getCopilotJoystick() {
+    return copilotController;
+  }
+
+  
+}
---
+++ src/main/java/frc/robot/commands/RunTankDrive.java
@@ -0,0 +1,40 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.Drive;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/**
+ *
+ */
+public class RunTankDrive extends CommandBase {
+    private final Drive subsystem;
+
+    public RunTankDrive(Drive subsystem) {
+        this.subsystem = subsystem;
+        // Use addRequirements() here to declare subsystem dependencies.
+        addRequirements(subsystem);
+      }
+    // Called just before this Command runs the first time
+    public void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    public void execute() {
+    	subsystem.tankDrive();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    public boolean isFinished() {
+    	return false;
+    }
+
+    // Called once after isFinished returns true
+    public void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    public void interrupted() {
+    	end();
+    }
+}

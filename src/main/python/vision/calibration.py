88d889f2506162a53353c213f6a1b6dd4e29dacd
Phillip Tran <ptrandev@protonmail.com>
2020-01-25 10:44:13 -0500

    set up calibration tool to read from network table

--- src/main/python/vision/calibration.py
+++ src/main/python/vision/calibration.py
@@ -4,25 +4,87 @@ import sys
 import cv2
 import numpy as np
 import configparser
+from cscore import CameraServer
 
 ##########
 # CONFIG #
 ##########
 
-# set up webcam capture source
-webcam = cv2.VideoCapture(0)
+# directory to save config file
+config_dir='config/'
 
 #####################################
+
+""" INIT CAMERA """
+
+# Init camera server
+cs = CameraServer.getInstance()
+cs.enableLogging()
+
+# Capture from the first USB Camera on the system
+camera = cs.startAutomaticCapture()
+camera.setResolution(320, 240)
+
+# Get a CvSink. This will capture images from the camera
+cvSink = cs.getVideo()
+
+# Setup a CvSource. This will send images back to the Dashboard
+outputStream = cs.putVideo("Processed Camera Feed", 320, 240)
+
+# Allocating new images is very expensive, always try to preallocate
+frame = np.zeros(shape=(240, 320, 3), dtype=np.uint8)
+
+print("[CALIBRATION]: Camera initialized.")
+
+""" INIT NETWORK TABLES """
+
+# set up network tables
+ntinst = NetworkTablesInstance.getDefault()
+ntinst.startClient("rei.local")
+table = ntinst.getTable("Calibration")
+
+print("[CALIBRATION]: Network Tables initialized.")
+
+""" CAMERA FEED """
+
+print("[CALIBRATION]")
+
+while True:
+    # Tell the CvSink to grab a frame from the camera and put it
+    # in the source image.  If there is an error notify the output.
+    time, frame = cvSink.grabFrame(frame)
+    if time == 0:
+        # Send the output the error.
+        outputStream.notifyError(cvSink.getError());
+        # skip the rest of the current iteration
+        continue
+
+    # Get values from calibrations
+    h_l = table.getNumber('H-lower')
+    v_l = table.getNumber('V-lower')
+    s_l = table.getNumber('S-lower')
+    h_u = table.getNumber('H-upper')
+    s_u = table.getNumber('S-upper')
+    v_u = table.getNumber('V-upper')
+
+    hsv_lower = np.array([h_l, s_l, v_l])
+    hsv_upper = np.array([h_u, s_u, v_u])
+
+    # apply hsv filter to image
+    frame = cv2.inRange(hsv, hsv_lower, hsv_upper)
+
+    # send some image back to the dashboard
+    outputStream.putFrame(frame)
+
+"""
 
 def callback(self):
     pass
 
-"""
 if not len(sys.argv) ==  2:
     print("[Calibration]: ERROR: calibration.py requires you to pass an image file as a command line argument.")
     print("	Please run: python calibration.py [path/to/file.format]")
     sys.exit()
-"""
 
 # Default blank image
 img = np.zeros((300,512,3), np.uint8)
@@ -85,3 +147,5 @@ while(1):
         break
 
 cv2.destroyAllWindows()
+
+"""
